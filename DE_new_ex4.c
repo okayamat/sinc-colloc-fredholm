04a03b418c296e88c47c20edd0fec0588ac216d2
Tomoaki Okayama <okayamat@gmail.com>
2025-02-27 23:42:28 +0900

    Added translate_fN function

--- DE_new_ex4.c
+++ DE_new_ex4.c
@@ -31,19 +31,19 @@ double u(double x)
   return 2*x/(1+ pow(x, 20));
 }
 
-double uDEn(double a, double b, double x, double h, int N, double* f_N, int n)
+double uDEn(double a, double b, double x, double h, int N, double* c_N, int n)
 {
   int j;
   double t = DE_trans_inv(a, b, x);
   double ans = 0;
 
   for (j = N; j > 0; j--) {
-    ans += (f_N[ j+N] - f_N[0]*waDE( j*h) - f_N[n-1]*wbDE( j*h)) * S( j, h, t);
-    ans += (f_N[-j+N] - f_N[0]*waDE(-j*h) - f_N[n-1]*wbDE(-j*h)) * S(-j, h, t);
+    ans += c_N[ j+N] * S( j, h, t);
+    ans += c_N[-j+N] * S(-j, h, t);
   }
-    ans += (f_N[ 0+N] - f_N[0]*waDE( 0  ) - f_N[n-1]*wbDE( 0  )) * S( 0, h, t);
+    ans += c_N[ 0+N] * S( 0, h, t);
 
-    ans += f_N[0]*wa(a, b, x) + f_N[n-1]*wb(a, b, x);
+    ans += c_N[n]*wa(a, b, x) + c_N[n+1]*wb(a, b, x);
 
   return ans;
 }
@@ -82,13 +82,24 @@ double* substitute_AN(double a, double b, double h, int N, int n, double* x_N)
 double* substitute_fN(int N, int n, double* x_N)
 {
   int i;
-  double* f_N = AllocVec(n);
+  double* f_N = AllocVec(n+2);
 
   for (i = -N; i <= N; i++) {
     f_N[i+N] = g(x_N[i+N]);
   }
 
   return f_N;
+}
+
+void translate_fN(double h, int N, int n, double* f_N)
+{
+  int j;
+  f_N[n+1] = f_N[n-1];
+  f_N[n]   = f_N[0];
+
+  for (j = N; j >= -N; j--) {
+    f_N[j+N] = f_N[j+N] - f_N[n]*waDE(j*h) - f_N[n+1]*wbDE(j*h);
+  }
 }
 
 int main()
@@ -118,6 +129,7 @@ int main()
     if ( info == 0 ) {
 
       maxerr = 0;
+      translate_fN(h, N, n, f_N);
 
       for (i = 1; i < SAMPLE; i++) {
         x = a + i*hh;
